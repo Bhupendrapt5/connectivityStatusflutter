bba648fcfa66892e0b164fbb99a3f2acac597c67
bhupendrapt95 <bhupendrapt95@gmail.com>
2021-01-07 23:01:56 +0530

    listening and modifying wifi status

--- android/app/src/main/kotlin/com/example/wifi_bluetooth_status/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wifi_bluetooth_status/MainActivity.kt
@@ -1,6 +1,66 @@
 package com.example.wifi_bluetooth_status
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.WifiManager
+import android.provider.Settings
+import android.util.Log
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.io.IOException
+
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "in.platform/system"
+    private var wifiStatus: Boolean = false
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            Log.i("info", "method call")
+            if (call.method =="changeWifi") {
+
+                    wifiStatus = call.arguments as Boolean
+                Log.i("info", "change wifi")
+                    changeWIFIStatus(wifiStatus, this.context)
+                }
+              else if  (call.method =="changeBluetooth") {
+                    changeBluetoothStatus()
+                }
+                else {
+                    result.run(MethodChannel.Result::notImplemented)
+                }
+            }
+
+    }
+
+    private fun changeWIFIStatus(newWifiStatus: Boolean, ctx: Context){
+
+        try {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q){
+                val panelIntent = Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
+                startActivityForResult(panelIntent, 545)
+            } else{
+                // do something for phones running an SDK before Q
+
+                val wifi = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+                wifi.setWifiEnabled(newWifiStatus)
+                Log.i("info", "wifi status ${wifi.isWifiEnabled}")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun changeBluetoothStatus(){
+       val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (mBluetoothAdapter.isEnabled) {
+            mBluetoothAdapter.disable()
+
+        } else {
+            mBluetoothAdapter.enable()
+        }
+    }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,59 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
+  static const platform = const MethodChannel('in.platform/system');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: getWifiStatus(),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('bottom'),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget getWifiStatus() {
+    return StreamBuilder(
+      builder: (BuildContext ctxt, AsyncSnapshot<ConnectivityResult> snapShot) {
+        if (!snapShot.hasData) return CircularProgressIndicator();
+        var result = snapShot.data == ConnectivityResult.wifi;
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              result ? Icons.wifi : Icons.wifi_off,
+              color: result ? Colors.blue : Colors.grey,
+              size: 60,
+            ),
+            Switch(
+              value: result,
+              onChanged: (value) async {
+                try {
+                  var getrs =
+                      await platform.invokeMethod('changeWifi', !result);
+                  print('result : $getrs');
+                } on PlatformException catch (e) {
+                  throw e;
+                }
+              },
+            )
+          ],
+        );
+      },
+      stream: Connectivity().onConnectivityChanged,
     );
   }
 }
